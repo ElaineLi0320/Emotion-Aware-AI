bc0a3f0da9ac3fc09622318be14d50a77a8a9a2c
Joey Cherisea <joeycullmann@Joeys-MacBook-Pro.local>
2025-03-02 18:32:12 -0800

    Write a script that uses our trained model to infer emotion labels live from webcam

--- live_infer.py
+++ live_infer.py
@@ -4,6 +4,9 @@ from Resnet import ResEmoteNet
 from pathlib import Path
 from torchvision import transforms
 import cv2
+import torch.nn.functional as F
+from PIL import Image
+import numpy as np
 
 # Configure Pytorch computation backend to either Nvidia GPUs or Apple silicon
 device = "cuda" if torch.cuda.is_available() else "mps"
@@ -18,8 +21,6 @@ model.eval()
 transform = transforms.Compose([
     # Faciliate the execution of SE Block
     transforms.Grayscale(num_output_channels=3),
-    # Introduces variability
-    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize(
         mean=[0.485, 0.456, 0.406],
@@ -38,14 +39,88 @@ font_color = (0, 255, 0)
 thickness = 3
 line_type = cv2.LINE_AA
 
+# Define a list of emotions based on its corresponding index position
+emotions = ['angry', 'frustration', 'boredom', 'happy', 'sad', 'surprise', 'neutral']
+
+max_emotion = ''
+def infer_emotion(frame):
+    """
+        Compute the probabilities of different emotions in an image frame
+    """
+    frame_tensor = transform(frame).unsqueeze(0).to(device)
+
+    # Run inference on image tensor without gradient computation
+    with torch.no_grad():
+        output = model(frame_tensor)
+        probs = F.softmax(output, dim=1)
+
+    # Convert tensors to numpy values
+    scores = probs.cpu().numpy().flatten()
+    rounded_scores = [round(score, 2) for score in scores]
+    return rounded_scores
+
+def infer_max_emotion(x, y, w, h,frame):
+    """
+        Employ our ML model to infer the most likely emotion in a video frame
+        
+        x,y: coordinates of top left point
+        w,h: width and height of an image frame
+        frame: a video frame
+    """
+    cropped_img = frame[y:y+h, x:x+w]
+    pil_img = Image.fromarray(cropped_img)
+    scores = infer_emotion(pil_img)
+    max_idx = np.argmax(scores)
+    return emotions[max_idx]
+
+
+def print_max_emotion(x, y, frame, emotion):
+    """
+        Display the most likely emotion label on video frames
+
+        emotion: string label to annote a frame
+    """
+    org = (x, y - 10)
+    cv2.putText(frame, emotion, org, font, font_scale, font_color, thickness, line_type)
+
+def print_all_emotion(x, y, w, h, frame):
+    """
+        Display all emotion labels and their probability score on video frames
+    """
+    cropped_img = frame[y:y+h, x:x+w]
+    pil_img = Image.fromarray(cropped_img)
+    all_scores = infer_emotion(pil_img)
+    org = (x+w+10, y-20)
+
+    for k, v in enumerate(emotions):
+        text = f"Label {v}: {all_scores[k]}"
+        y = org[1] + 40
+        org = (org[0], y)
+        cv2.putText(frame, text, org, font, font_scale, font_color, thickness, line_type)
+
 def detect_bounding_box(frame, counter):
     """
-        Detect a bounding box around faces in an image frame
+        Detect faces in a video stream
 
         frame: an image frame
         counter: an integer controlling when to invoke our ML model
     """
-    pass
+    global max_emotion
+    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray_img, 1.1, 5, minSize=(40, 40))
+
+    for x, y, w, h in faces:
+        # Draw a bounding box around a face
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # Infer emotion on defined interval
+        if counter == 0:
+            max_emotion = infer_max_emotion(x, y, w, h, frame)
+
+        print_max_emotion(x, y, frame, max_emotion)
+        print_all_emotion(x, y, w, h, frame)
+
+    return faces
 
 # Access live webcam feed
 video_capture =cv2.VideoCapture(0)
